316fa2544802f2913d82659423a05616abebaff7
ANJALI-boya <229x1a3225@gprec.ac.in>
2025-05-26 22:42:00 +0530

    Create NumberGuessingGame.java

---
+++ JAVA/NumberGuessingGame.java
@@ -0,0 +1,17 @@
+// 10. Number Guessing Game
+import java.util.*;
+
+public class NumberGuessingGame {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numberToGuess = new Random().nextInt(100) + 1;
+        int guess;
+        do {
+            System.out.print("Guess a number between 1 and 100: ");
+            guess = scanner.nextInt();
+            if (guess < numberToGuess) System.out.println("Too low");
+            else if (guess > numberToGuess) System.out.println("Too high");
+            else System.out.println("Correct!");
+        } while (guess != numberToGuess);
+    }
+}
