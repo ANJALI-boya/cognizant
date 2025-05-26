c4b881a582249fbc84d6ab6d20d061bdeb9f69fb
ANJALI-boya <229x1a3225@gprec.ac.in>
2025-05-26 23:33:36 +0530

    Create StringUtils.java

---
+++ JAVA/Util/StringUtils.java
@@ -0,0 +1,13 @@
+module com.utils {
+    exports com.utils;
+}
+package com.utils;
+
+public class StringUtils {
+    public static String capitalize(String text) {
+        if (text == null || text.isEmpty()) {
+            return text;
+        }
+        return Character.toUpperCase(text.charAt(0)) + text.substring(1);
+    }
+}
