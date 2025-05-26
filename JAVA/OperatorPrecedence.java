0bc232746690322b1b907ed29aec57ab8137a6ae
ANJALI-boya <229x1a3225@gprec.ac.in>
2025-05-26 22:40:36 +0530

    Create OperatorPrecedence.java

---
+++ JAVA/OperatorPrecedence.java
@@ -0,0 +1,8 @@
+// 8. Operator Precedence
+public class OperatorPrecedence {
+    public static void main(String[] args) {
+        int result = 10 + 5 * 2;
+        System.out.println("Result: " + result);
+    }
+}
+
